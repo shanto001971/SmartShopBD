7ef0072de096a4c4ee918e1ce79d14d0795235d2
Shanto Islam <rajs45693@gmail.com>
2024-02-03 00:33:10 +0600

    implement custome aut api in seller page

--- src/hooks/useSeller.jsx
+++ src/hooks/useSeller.jsx
@@ -1,27 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure from "./useAxiosSecure";
-import { useContext } from "react";
-import { AuthContext } from "../Provider/AuthProvider";
+// import { useQuery } from "@tanstack/react-query";
+// import useAxiosSecure from "./useAxiosSecure";
+// import { useContext } from "react";
+// import { AuthContext } from "../Provider/AuthProvider";
 
-const useSeller = (email) => {
-    const { user, loading } = useContext(AuthContext);
-    const [axiosSecure] = useAxiosSecure();
+// const useSeller = (email) => {
+//     const { user, loading } = useContext(AuthContext);
+//     const [axiosSecure] = useAxiosSecure();
 
-    const { data: isSeller, isLoading: isSellerLoading } = useQuery({
-        queryKey: ['isSeller', user?.email],
-        enabled: !loading,
-        queryFn: async () => {
-            try {
-                const res = await axiosSecure.get(`/users/seller/${user?.email}`);
-                return res.data.seller;
-            } catch (error) {
-                console.error('Error checking seller status:', error);
-                throw error;
-            }
-        }
-    });
+//     const { data: isSeller, isLoading: isSellerLoading } = useQuery({
+//         queryKey: ['isSeller', user?.email],
+//         enabled: !loading,
+//         queryFn: async () => {
+//             try {
+//                 const res = await axiosSecure.get(`/users/seller/${user?.email}`);
+//                 return res.data.seller;
+//             } catch (error) {
+//                 console.error('Error checking seller status:', error);
+//                 throw error;
+//             }
+//         }
+//     });
 
-    return [isSeller, isSellerLoading];
-};
+//     return [isSeller, isSellerLoading];
+// };
 
-export default useSeller;
+// export default useSeller;
